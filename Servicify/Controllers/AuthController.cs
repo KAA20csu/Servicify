609abb64fb8c1544600f7fcce22a324e2ff67533
Omaenoshi <rassvetalov.maksim@mail.ru>
2023-11-29 03:51:32 +0500

    feat: added login form

---
+++ Servicify/Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Servicify.Controllers;
+
+[Route("/auth")]
+public class AuthController : Controller
+{
+    [Route("login")]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    
+    [Route("signup")]
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
