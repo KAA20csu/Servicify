7df262cdfd6b0fb1bbfb80c3fad242c1b2f1401a
Omaenoshi <rassvetalov.maksim@mail.ru>
2023-11-30 03:43:16 +0500

    feat: added serviceCard and view organization

---
+++ Servicify/Models/ServiceViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Servicify.Models;
+
+public class ServiceViewModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Duration { get; set; }
+    public float Cost { get; set; }
+
+    public ServiceViewModel(long id, string name, string duration, float cost)
+    {
+        Id = id;
+        Name = name;
+        Duration = duration;
+        Cost = cost;
+    }
+}
